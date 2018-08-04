cd05a05e4e0eb9da76d82a379d3d2a0347cc869d
Haley <hjsulcer@gmail.com>
2018-08-03 23:40:20 -0400

    added basic grid to body component and fixed arrows a little

--- devver/src/Body/Body.js
+++ devver/src/Body/Body.js
@@ -5,13 +5,43 @@ const Body = () => (
 
   <div className="body">
 
-    <div className="arrows">
+    <div className="arrow-l">
       <img src={window.location.origin + '/images/left-arrow.png'} alt="left-a" />
-      <img src={window.location.origin + '/images/right-arrow.png'} alt="right-a" />
     </div>
 
 
-  </div>
+    <div className="grid">
+
+      <div className="row-1">
+        <div className="box">1</div>
+        <div className="box">2</div>
+        <div className="box">3</div>
+        <div className="box">4</div>
+      </div>
+
+      <div className="row-2">
+        <div className="box">5</div>
+        <div className="box">6</div>
+        <div className="box">7</div>
+        <div className="box">8</div>
+      </div>
+
+    <div className="row-3">
+      <div className="box">9</div>
+      <div className="box">10</div>
+      <div className="box">11</div>
+      <div className="box">12</div>
+    </div>
+
+  </div> 
+
+    <div className="arrow-r">
+      <img src={window.location.origin + '/images/right-arrow.png'} alt="right-a" />
+    </div>
+
+</div>
+
+
 )
 
 export default Body;
--- devver/src/Footer/Footer.js
+++ devver/src/Footer/Footer.js
@@ -3,14 +3,13 @@ import './Footer.css';
 
 const Footer = () => (
   <div className="footer">
-    <h1>Footer</h1>
+  
       <ul className="footer-links">
         <li>Interview Prep</li>
         <li>Algorithm Practice</li>
         <li>Job Boards</li>
         <li>News and Forums</li>
       </ul>
-
 
   </div>
 )
