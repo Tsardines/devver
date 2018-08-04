8fab85fdaf117727dcae19038f4145ce9afdbe07
Haley Sulcer <hjsulcer@gmail.com>
2018-08-04 17:47:26 -0400

    added hamburger placeholders in grid

--- devver/src/Body/Body.js
+++ devver/src/Body/Body.js
@@ -13,27 +13,57 @@ const Body = () => (
     <div className="grid">
 
       <div className="row-1">
-        <div className="box">1</div>
-        <div className="box">2</div>
-        <div className="box">3</div>
-        <div className="box">4</div>
+
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+
       </div>
 
       <div className="row-2">
-        <div className="box">5</div>
-        <div className="box">6</div>
-        <div className="box">7</div>
-        <div className="box">8</div>
+
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+        <div className="box">
+          <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+        </div>
+
       </div>
 
     <div className="row-3">
-      <div className="box">9</div>
-      <div className="box">10</div>
-      <div className="box">11</div>
-      <div className="box">12</div>
+
+      <div className="box">
+        <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+      </div>
+      <div className="box">
+        <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+      </div>
+      <div className="box">
+        <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+      </div>
+      <div className="box">
+        <img className="brg" src={window.location.origin + "/images/burger.png"} alt="brg" />
+      </div>
+
     </div>
 
-  </div> 
+  </div>
 
     <div className="arrow-r">
       <img src={window.location.origin + '/images/right-arrow.png'} alt="right-a" />
--- devver/src/Footer/Footer.js
+++ devver/src/Footer/Footer.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => (
+
   <div className="footer">
-  
+
+
+    <div className="left-box">
       <ul className="footer-links">
         <li>Interview Prep</li>
         <li>Algorithm Practice</li>
         <li>Job Boards</li>
         <li>News and Forums</li>
       </ul>
+    </div>
+
+    <div className="right-box">
+
+    </div>
 
   </div>
 )
