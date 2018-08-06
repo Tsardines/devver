f0f39057b11fa9429770c233c2356747845fd0ff
Haley Sulcer <hjsulcer@gmail.com>
2018-08-06 17:43:22 -0400

    installed routes and made Algo.js layout the same as IntPrep.js

--- devver/src/App.js
+++ devver/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 import Header from './Header/Header';
-import IntPrep from './1IntPrep/IntPrep';
+import IntPrep from './1IntPrep/IntPrep.js';
 import Algo from './2Algo/Algo';
+import Home from './3Home/Home';
 import Footer from './Footer/Footer';
 
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="Everything">
-        <Header />
-        <IntPrep />
-        <Algo />
-        <Footer />
-      </div>
+      <div>
+      <Header />
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Home} />
+          <Route path="/interview-prep" component={IntPrep} />
+          <Route path="/algorithm-practice" component={Algo} />
+        </div>
+      </BrowserRouter>
+    </div>
     );
   }
 }
--- devver/src/Header/Header.js
+++ devver/src/Header/Header.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './Header.css';
-import IntPrep from './1IntPrep/IntPrep';
-import Algo from './2Algo/Algo';
+
 
 const Header = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-        </li>
-        <li>
-          <Link to="/interview-prep">Interview Prep</Link>
-        </li>
-        <li>
-          <Link to="/algorithm-practice">Algorithm Practice</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route path="/interview-prep" component={IntPrep} />
-      <Route path="/algorithm-practice" component={Algo} />
-    </div>
-  </Router>
+  <div></div>
 )
 
 export default Header;
